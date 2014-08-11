ecd1b54d56dba84a7ece8dbfa095486c643dcee6
Sinbad840628 <Sinbad840628@gmail.com>
2014-08-12 00:08:38 +0800

    更新类文件中的方法

--- src/com/java/ui/MediaPlayer.java
+++ src/com/java/ui/MediaPlayer.java
@@ -1,5 +1,6 @@
 package com.java.ui;
 
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -16,11 +17,25 @@ public class MediaPlayer extends JFrame implements ActionListener,ControllerEven
 	 * @author Sinbad
 	 * @since 2014.08.11
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	//MediaPlayer类的变量成员的声明
+	
+	//构造函数，其中包括了设置响应窗口事件的监听器；
+	public MediaPlayer(String title) {
+		// TODO Auto-generated constructor stub
 	}
+	
+	//本方法是用来设置程序菜单栏的
+	public void setupMenu(){
+		
+	}//end of setupMenu
+	
+	public void controllerUpdate(ControllerEventListener e){
+		
+	}
+	
 
+	//菜单状态改变事件的响应函数
 	@Override
 	public void itemStateChanged(ItemEvent e) {
 		// TODO Auto-generated method stub
@@ -33,10 +48,25 @@ public class MediaPlayer extends JFrame implements ActionListener,ControllerEven
 		
 	}
 
+	//动作时间响应成员；捕捉发送到本对象的各个事件
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	//调用绘图函数进行界面的绘制
+	public void update(Graphics g){
+		
+	}
+	
+	public void paint(Graphics g){
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
